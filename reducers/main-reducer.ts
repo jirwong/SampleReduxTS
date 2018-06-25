ea0a2ad7d5a1cca020c135a09b2dc706be15a069
Wong Horng Jir <jirwong@gmail.com>
2018-06-25 15:34:18 +0800

    Branch with undo

--- reducers/main-reducer.ts
+++ reducers/main-reducer.ts
@@ -5,7 +5,7 @@ import undoable from 'redux-undo';
 
 export const mainReducer: Reducer = combineReducers({
     // Uncomment the below for redux undo
-    //baseTotal : undoable(baseTotal),
-    //baseMessages : undoable(baseMessages)
-    baseTotal, baseMessages
+    baseTotal : undoable(baseTotal),
+    baseMessages : undoable(baseMessages)
+    //baseTotal, baseMessages
 });
